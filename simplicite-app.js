405e1e208bfb2d8fc55e7e48e4b8378c29ccb8d9
dazoulay <dazoulay@simplicite.fr>
2014-10-17 22:32:02 +0200

    Updated

---
+++ simplicite-app.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+	function SimpliciteAppNode(n) {
+		RED.nodes.createNode(this, n);
+		this.host = n.host;
+		this.port = n.port;
+		this.root = n.root;
+		this.login = n.login;
+		this.password = n.password;
+	}
+	RED.nodes.registerType("simplicite-app", SimpliciteApp);
+}
