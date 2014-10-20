ee2c765d2fde87ddc49a47aa07ba74f27882c58b
dazoulay <dazoulay@simplicite.fr>
2014-10-20 10:31:45 +0200

    Fixed

--- simplicite-info.js
+++ simplicite-info.js
@@ -2,13 +2,13 @@ module.exports = function(RED) {
 	function SimpliciteInfo(config) {
 		RED.nodes.createNode(this,config);
 		var node = this;
-		this.on("input", function(msg) {
-			this.server = RED.nodes.getNode(config.server);
-			if (this.server) {
-				this.server.session.getAppInfo().then(function(appinfo) {
+		node.on("input", function(msg) {
+			node.server = RED.nodes.getNode(config.server);
+			if (node.server) {
+				node.server.session.getAppInfo().then(function(appinfo) {
 					msg.payload = { application: appinfo };
-					this.server.session.getSysInfo().then(function(sysinfo) {
-						msg.payload.system = appinfo;
+					node.server.session.getSysInfo().then(function(sysinfo) {
+						msg.payload.system = sysinfo;
 						node.send(msg);
 					});
 				});
