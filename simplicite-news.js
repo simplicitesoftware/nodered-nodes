54f9c7cf011175ba36a7c4f306a9aa326c08e11c
dazoulay <dazoulay@simplicite.fr>
2014-10-20 11:23:59 +0200

    Changed name

--- simplicite-news.js
+++ simplicite-news.js
@@ -1,11 +1,12 @@
 module.exports = function(RED) {
 	function SimpliciteNews(config) {
-		RED.nodes.createNode(this,config);
+		RED.nodes.createNode(this, config);
 		var node = this;
 		this.on("input", function(msg) {
+			var params = msg.payload;
 			this.server = RED.nodes.getNode(config.server);
 			if (this.server) {
-				this.server.session.getNews().then(function(news) {
+				this.server.session.getNews({ inlineImages: params && params.images == "true" }).then(function(news) {
 					msg.payload = news;
 					node.send(msg);
 				});
