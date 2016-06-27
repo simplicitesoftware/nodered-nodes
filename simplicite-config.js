e1a0bf937173b30240ab71e0a5b3404e7b71cf36
David AZOULAY <dazoulay@simplicite.fr>
2016-06-27 11:38:51 +0200

    Version 0.0.10

--- simplicite-config.js
+++ simplicite-config.js
@@ -5,9 +5,9 @@ module.exports = function(RED) {
 	var simpliciteService = appEnv.services.simplicite && appEnv.services.simplicite.length > 0 ? appEnv.services.simplicite[0].credentials : {};
 	//console.log("Simplicite service = " + JSON.stringify(simpliciteService, null, 2));
 
-	/*RED.httpAdmin.get('/simplicite-service', function(req, res) {
+	RED.httpAdmin.get('/simplicite-service', function(req, res) {
 		res.send(JSON.stringify(simpliciteService));
-	});*/
+	});
 
 	function SimpliciteConfig(n) {
 		RED.nodes.createNode(this, n);
