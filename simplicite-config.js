7fa057f744b001b1ed41de462534b5eddad2e60c
David AZOULAY <dazoulay@simplicite.fr>
2022-03-17 11:24:37 +0100

    Simplified

--- simplicite-config.js
+++ simplicite-config.js
@@ -1,9 +1,7 @@
 module.exports = function(RED) {
 	var cfEnv = require('cfenv');
 	var appEnv = cfEnv.getAppEnv();
-	//console.log('Application environment = ' + JSON.stringify(appEnv, null, 2));
 	var simpliciteService = appEnv.services.simplicite && appEnv.services.simplicite.length > 0 ? appEnv.services.simplicite[0].credentials : {};
-	//console.log('Simplicite service = ' + JSON.stringify(simpliciteService, null, 2));
 
 	RED.httpAdmin.get('/simplicite-service', function(req, res) {
 		res.send(JSON.stringify(simpliciteService));
@@ -11,7 +9,8 @@ module.exports = function(RED) {
 
 	function SimpliciteConfig(n) {
 		RED.nodes.createNode(this, n);
-		var params = {
+
+		this.session = require('simplicite').default.session({
 			host: n.host != '' ? n.host : simpliciteService.host,
 			port: n.port != '' ? parseInt(n.port) : simpliciteService.port,
 			root: n.root != '' ? n.root : simpliciteService.root,
@@ -19,9 +18,8 @@ module.exports = function(RED) {
 			username: n.username != '' ? n.username : 'public',
 			password: n.password != '' ? n.password : 'simplicite',
 			debug: n.debug == 'true'
-		};
-		//console.log('Simplicite session parameters = ' + JSON.stringify(params, null, 2));
-		this.session = require('simplicite').default.session(params);
+		});
 	}
+
 	RED.nodes.registerType('simplicite-config', SimpliciteConfig);
 };
