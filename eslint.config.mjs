8410a9bab80ab849cba1d254d0165a5949bd2982
David AZOULAY <dazoulay@simplicite.fr>
2024-12-10 16:52:37 +0100

    Added ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,11 @@
 export default [{
-    languageOptions: {
-        ecmaVersion: 11,
-        sourceType: "module",
-    },
     rules: {
         "indent": ["error", "tab"],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "single"],
         "semi": ["error", "always"],
         "no-multiple-empty-lines": ["error", { max: 1 }],
+        "no-multi-spaces": "error",
         "no-trailing-spaces": [ "error", { skipBlankLines: false } ]
     }
 }];
