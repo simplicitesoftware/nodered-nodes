1baf85fa1417da8fdea698720397045366f1d229
David AZOULAY <dazoulay@simplicite.fr>
2024-12-10 16:44:08 +0100

    Simplified ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,14 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
 export default [{
-    ignores: ["**/simplicite.js"],
-}, ...compat.extends("eslint:recommended"), {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-            ...globals.node,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-        },
-
         ecmaVersion: 11,
-        sourceType: "commonjs",
+        sourceType: "module",
     },
-
     rules: {
         "indent": ["error", "tab"],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "single"],
         "semi": ["error", "always"],
-    },
+        "no-multiple-empty-lines": ["error", { max: 1 }],
+        "no-trailing-spaces": [ "error", { skipBlankLines: false } ]
+    }
 }];
