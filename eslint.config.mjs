d4758a4577983b4e9ae3571ef76191d07be1f2dd
David AZOULAY <dazoulay@simplicite.fr>
2024-12-19 13:11:39 +0100

    Improved ESLint linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
-export default [{
-  rules: {
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'no-multiple-empty-lines': ['error', { max: 1 }],
-    'no-multi-spaces': 'error',
-    'no-trailing-spaces': [ 'error', { skipBlankLines: false } ],
-    'no-console': 'error',
-    'no-debugger': 'error'
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-multi-spaces': 'error',
+      'no-trailing-spaces': [ 'error', { skipBlankLines: false } ],
+      'no-console': 'error',
+      'no-debugger': 'error'
+    }
   }
-}];
+];
